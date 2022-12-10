d33ebae4e29f150aa4f7980e0b9ad29f0e985528
Amaechi Chisom <amaechilegend@gmail.com>
2022-12-10 07:23:14 +0100

    first commit

---
+++ assets/js/submitPost.js
@@ -0,0 +1,32 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+const form = document.querySelector("#make-post");
+
+
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+    // generating id
+    let id = Math.floor(Math.random() * 1000);
+
+    const formData = new FormData();
+
+    formData.append("name", form.elements["name"].value)
+    formData.append("title", form.elements["title"].value)
+    formData.append("image", form.elements["image"].value)
+
+    fetch(`https://61924d4daeab5c0017105f1a.mockapi.io/credo/v1/news/${id}`, {
+        method: 'post',
+        body: formData,
+    }).then((data) => {
+        console.log(data)
+        alert("post made")
+        location.reload();
+    })
+    
+});
+
